1c62a234dadc1c927b4103da6b1d4233c9fd7b34
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-05-11 19:50:07 -0700

    added an error page JS function

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@
 const REGISTRATION_URL = 'http://127.0.0.1:3000/registration';
 const LOGIN_URL = 'http://127.0.0.1:3000/login';
 const LOGOUT_URL = 'http://127.0.0.1:3000/logout';
-const PASSWORD_MIN = 1;
+const PASSWORD_MIN = 8;
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -20,14 +20,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
             event.preventDefault();
             const userEmail = document.getElementsByName('userEmail')[0];
             const userPass = document.getElementsByName('userPass')[0];
-            check_for_empty(userEmail, userPass);
-            let response = await registration_and_login_fetch(userEmail.value, userPass.value, LOGIN_URL);
-            let data = await response.json();
-            if(data.message == 'session start'){
-                window.location.assign('userhome.html');
-            }
-            else{
-                window.alert("wrong email and/or password");
+            if(!(check_for_empty(userEmail, userPass))){
+                let response = await registration_and_login_fetch(userEmail.value, userPass.value, LOGIN_URL);
+                console.log(response)
+                if(response != null){
+                    let data = await response.json();
+                    if(data.message == 'session start'){
+                        window.location.assign('userhome.html');
+                    }
+                    else{
+                        error_message("wrong email and/or password");
+                    }
+                }
             }
         });
     }
@@ -52,22 +56,82 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const strip_body = function(){
+    const body = document.body;
+    const oldBody = [];
+    while(body.children.length > 1){
+        oldBody.push(body.lastChild);
+        body.removeChild(body.lastChild);
+    }
+    return oldBody;
+}
+
+const restore_body = function(oldBody){
+    const body = document.body;
+    for(let i = (oldBody.length - 1); i >= 0; i--){
+        body.appendChild(oldBody[i]);
+    }
+}
+
+const styled_container = function(){
+    const res = document.createElement('div');
+    const styler = res.style;
+    styler.width = '30%'
+    styler.display = 'flex';
+    styler.justifyContent = 'center';
+    styler.alignItems = 'center';
+    styler.height = 'auto';
+    styler.backgroundColor = 'var(--strong-blue)';
+    styler.borderRadius = '20px';
+    styler.marginTop = "30vh";
+    styler.color = 'var(--yellow-gold)';
+    styler.fontSize = '250%';
+    styler.textAlign = 'center';
+    styler.padding = '3%';
+    return res;
+}
+
+let error_message = function(message){
+    let timer = 2000;
+    const oldBody = strip_body();
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = message;
+    if(message.length > 40){
+        timer = timer + 1000;
+    }
+    const messageContainer = styled_container();
+    messageContainer.appendChild(errorMessage);
+
+    document.body.appendChild(messageContainer);
+    setTimeout(()=>{
+        document.body.removeChild(document.body.lastChild);
+        restore_body(oldBody);
+    }, timer);
+    
+}
+
+
+
+
 const process_signup_data = async(event)=>{
     event.preventDefault();
     const email = document.getElementsByName('email')[0];
     const pass = document.getElementsByName('pass')[0];
     const passConfirm = document.getElementsByName('passConfirm')[0];
     const validPassword = password_validation(email, pass, passConfirm);
+    console.log(validPassword);
+
 
     if(validPassword != 0){
-        console.log("A");
         console.log(email.value, pass.value);
         let response = await registration_and_login_fetch(email.value, pass.value, REGISTRATION_URL);
         if(response != null){
             inform_user(response);
         }
         else{
-            window.alert("there was an error trying to make an account for you\n please try again");
+            error_message("there was an error trying to make an account for you, please try again");
         }
     }
 }
@@ -86,16 +150,27 @@ const inform_user = async(response)=>{
 
 
 const password_validation = function(email, pass, passConfirm){
-    check_for_empty(email, pass);
-    if(Array.from(pass.value).length < PASSWORD_MIN){
-        window.alert("passwords must be at least 8 characters long!");
-        return 0;
+    const isEmpty = check_for_empty(email, pass);
+    console.log(isEmpty);
+
+    if(isEmpty != 1){
+        console.log(Array.from(pass.value).length);
+
+        if(Array.from(pass.value).length < PASSWORD_MIN){
+            error_message("passwords must be at least 8 characters long!");
+            return 0;
+        }
+        if(pass.value !== passConfirm.value){
+            console.log(passConfirm.value);
+
+            error_message("passwords must match!");
+            return 0;
+        }
+        else{
+            return 1;
+        }
     }
-    if(pass.value !== passConfirm.value){
-        window.alert("passwords must match!");
-        return 0;
-    }
-    return 1;
+    return 0;
 }
 
 const registration_and_login_fetch = async(email, pass, endpoint)=>{
@@ -110,16 +185,20 @@ const registration_and_login_fetch = async(email, pass, endpoint)=>{
         });
         return response;
     }catch(error){
-        console.log("\nFETCH ERROR");
+        console.log(error);
+        error_message("There seems to be an issue connecting to backend web services at the moment :/")
         return null;
     }
 }
 
 const check_for_empty = function(email, pass){
     if(email && pass){
+        console.log(email.value, pass.value);
         if(email.value == "" || pass.value == ""){
-            window.alert("please fill out the whole form!");
-            return;
+            error_message("Please fill out the entire form!");
+            return 1;
         }
+        return 0;
     }
+    return 1;
 }

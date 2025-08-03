3c36c5be88d811dc4379b2a544d4860a08d9e806
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-08-02 23:57:05 -0700

    prototype

--- prototypes/project-card.js
+++ prototypes/project-card.js
@@ -3,25 +3,47 @@
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
-    const toggle_project_resource_height = function(projectResourcesList, i){
-        const numChildren = projectResourcesList[i].children.length;
-        const newHeight = numChildren * 4;
-        if(projectResourcesList[i].style.maxHeight == '0vh'){
-            projectResourcesList[i].style.maxHeight = newHeight + 'vh';
-        }
-        else{
-            projectResourcesList[i].style.maxHeight = '0vh';
-        }
-        
+    expanded_list_functionality('toggle-project-resources', 'project-resources');
+
+    expanded_list_functionality('toggle-project-steps', 'project-steps');
+
+});
+
+
+
+const toggle_list_height = function(projectResourcesList, i){
+    /* 
+    DESCRIPTION: Function defined to re-assign the max height of the given 
+                list to expand or contract on click
+    INPUT(S): A list of project resource sets (array), and index in the list (integer)
+    OUTPUT(S): None
+    */
+    const numChildren = projectResourcesList[i].children.length;
+    const heightOfSingleLink = projectResourcesList[i].children[0].children[0].getBoundingClientRect().height;
+    const newMaxHeight = (numChildren * heightOfSingleLink) + 10;
+    console.log(getComputedStyle(projectResourcesList[i]).maxHeight)
+    if(getComputedStyle(projectResourcesList[i]).maxHeight == '0px'){
+        projectResourcesList[i].style.maxHeight = newMaxHeight + 'px';
+        return;
     }
-    let expandProjectResourcesButtonList = document.getElementsByClassName('toggle-project-resources');
-    if(expandProjectResourcesButtonList){
-        expandProjectResourcesButtonList = Array.from(expandProjectResourcesButtonList);
-        const projectResourcesList = Array.from(document.getElementsByClassName('project-resources'));
-        for(let i = 0; i < expandProjectResourcesButtonList.length; i++){
-            expandProjectResourcesButtonList[i].addEventListener('click', ()=>{
-                toggle_project_resource_height(projectResourcesList, i);
+    projectResourcesList[i].style.maxHeight = '0px';
+}
+
+const expanded_list_functionality = function(buttonClassName, containerClassName){
+    /* 
+    DESCRIPTION: Function defined to access all the expansion buttons on the page and apply event listeners
+    INPUT(S): None
+    OUTPUT(S): None
+    */
+    let expandProjectButtonList = document.getElementsByClassName(buttonClassName);
+    if(expandProjectButtonList){
+        expandProjectButtonList = Array.from(expandProjectButtonList);
+        const projectList = Array.from(document.getElementsByClassName(containerClassName));
+        for(let i = 0; i < expandProjectButtonList.length; i++){
+            expandProjectButtonList[i].addEventListener('click', ()=>{
+                toggle_list_height(projectList, i);
             });
         }
     }
-})
+
+}

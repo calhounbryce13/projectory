9166fe1ed8f1493e3105e0ea1b690653d9ee6004
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-08-07 21:34:53 -0700

    housekeeping with prototypes, testing lottie loading screen animations

---
+++ prototypes/loading_icon/loading_icon.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+
+    lottie.loadAnimation({
+        container: document.getElementById('testing-lottie-animation'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'loading_icon.json'
+    });
+
+});
+
+
+const show_loading = function(){
+
+}
+
+const dismiss_loading = function(){
+
+}

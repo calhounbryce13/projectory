1297d6fd19d2f040597cf2dc573cf9f1849196fe
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-11-17 06:08:02 -0800

    updates

--- script/homepage_script.js
+++ script/homepage_script.js
@@ -3,8 +3,6 @@ import { endpoints } from 'endpoints.js';
 
 
 document.addEventListener("DOMContentLoaded", async() => {
-
-
     try{
         const response = await fetch(endpoints.get_nums, {method: 'GET'});
         if(response.status == 200){

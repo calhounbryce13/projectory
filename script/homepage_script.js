a8ccc9261e969d4bd0d2305cc43c38992d367883
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-11-17 16:18:09 -0800

    Update homepage_script.js

--- script/homepage_script.js
+++ script/homepage_script.js
@@ -7,9 +7,20 @@ document.addEventListener("DOMContentLoaded", async() => {
         const response = await fetch(endpoints.get_nums, {method: 'GET', credentials: 'include'});
         if(response.status == 200){
             const data = await response.json();
-            console.log(data);
+            populate_labels(data);
         }
     }catch(error){
         console.log(error);
     }
-})
+});
+
+
+
+const populate_labels = function(data){
+    const plannedButton = document.getElementById('planned');
+    const currentButton = document.getElementById('planned');
+    const completeButton = document.getElementById('planned');
+    plannedButton.children[1].children[1].textContent = data[0];
+    currentButton.children[1].children[1].textContent = data[1];
+    completeButton.children[1].children[1].textContent = data[2];
+}
